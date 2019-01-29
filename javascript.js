fd0a20dff61a581e0be59c36822b005604ecec1d
Tengil91 <kachiun91@gmail.com>
2019-01-29 15:50:45 +0100

    .

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let rectcanvas = document.getElementById('rectcanvas');
 let ctx = rectcanvas.getContext('2d');
-let xstart = 20, ystart = 40, xsize = 50, ysize = 100;
+let xstart = 200, ystart = 40, xsize = 50, ysize = 100;
 ctx.rect(xstart, ystart, xsize, ysize);
 ctx.stroke();
