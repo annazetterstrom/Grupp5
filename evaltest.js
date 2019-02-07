6201e112bab2b2ab19eff60793b82dc737845a02
Tengil91 <kachiun91@gmail.com>
2019-02-07 16:33:24 +0100

    removed testfiles

--- evaltest.js
+++
@@ -1,8 +0,0 @@
-let codediv = document.getElementById('code1');
-let button = document.getElementById('button');
-button.addEventListener('click', evaluate);
-
-function evaluate(){
-    let code = codediv.value;
-    eval(code);
-}
