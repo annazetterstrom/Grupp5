b0c1bf50e5b6235c684e126cafb4abb74e2d9c2e
Frida Schoultz <fridaschoultz@hotmail.com>
2019-02-08 12:36:55 +0100

    la till annan färg på animeringen på hem

--- froggs.js
+++ froggs.js
@@ -39,12 +39,12 @@ function Smiley(x, y, xv, yv, r, color){
         cty.arc(this.x+this.r/2, this.y-this.r/2, this.r/2, 0, 2*Math.PI);
         cty.fill();
         cty.beginPath();
-        cty.fillStyle = 'red';
+        cty.fillStyle = 'white';
         cty.arc(this.x,this.y+this.r/4,this.r/2, 0, Math.PI);
         cty.fill();
     }
 }
-let smilies = [new Smiley(150,60,0,0,40,'hotpink'), new Smiley(20,110,1,0,20,'green'), new Smiley(70,100,1,0,20,'orange'), new Smiley(200,105,1,0,20,'green'), new Smiley(250,100,1,0,20,'blue')];
+let smilies = [new Smiley(150,60,0,0,40,'lightcoral'), new Smiley(20,110,1,0,20,'cadetblue'), new Smiley(70,100,1,0,20,'burlywood'), new Smiley(200,105,1,0,20,'burlywood'), new Smiley(250,100,1,0,20,'cadetblue')];
 animateSmiley();
 
 function animateSmiley(){
